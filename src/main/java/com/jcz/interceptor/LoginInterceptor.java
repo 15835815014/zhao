724b970dd7a500a52137a3f8dcfa29e12eb0fdec
lenovo <lenovo@wz-zy>
2017-12-06 16:46:28 +0800

    赵

---
+++ src/main/java/com/jcz/interceptor/LoginInterceptor.java
@@ -0,0 +1,9 @@
+package com.jcz.interceptor;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class LoginInterceptor extends HandlerInterceptorAdapter{
+	
+	
+
+}
